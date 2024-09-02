1c3708f9b349ef062032959203b0fe3f011dc8b7
Anas-kj <anaskhouajaa@gmail.com>
2024-09-02 09:13:20 +0800

    update navbar

---
+++ src/app/components/Button/ButtonSecondary.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button"
+ 
+export function ButtonSecondary() {
+  return <Button variant="secondary" className="max-w-[200px] m-auto">About Us</Button>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
 "use client";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import wind from "../../public/wind.jpg";
+import Image from "next/image";
+import { ButtonSecondary } from "./components/Button/ButtonSecondary";
 
 export default function Home() {
-  const router = useRouter();
+
   return (
     <main>
-      <h1>Home</h1>
-      <button className="block text-red-50" onClick={() => router.push("/about")}>Click Button</button>
+      <section className="relative "> {/* hero section */}
+        <Image src={wind} alt="wind turbine" className="w-full"/>
+        {/* <h1 className="absolute top-1/2 text-3xl font-bold ">Protecting the Environment for a Sustainable Future</h1>
+        <p className="absolute top-60">EAM is committed to environmental protection, providing impactful solutions for a greener world.</p> */}
+      </section>
+
+      <section className="flex flex-col gap-4 lg:flex-row  items-center py-6"> {/* about section */}
+        <Image src={wind} alt="wind turbine" className="w-2/3 rounded-lg lg:w-1/2"/>
+        <div className="flex flex-col text-center p-6 max-w-full">
+            <h2 className="text-3xl text-bold">
+              Experience 26+ years of leading the environment industry
+            </h2>
+            <p className="text-md text-center">
+            At EAM, we have built a strong reputation based on our extensive 
+            experience in the environmental sector. With over 25 years of 
+            industry-leading expertise, we are the go-to company for all your environmental needs.
+            </p>
+            <ButtonSecondary/>
+        </div>
+        
+      </section>
+
     </main>
   );
 }
